a95466a83f7b3bfea4e1cead111e54f2cda52aff
pettijohn@github <pettijohn@github>
2022-11-22 09:57:41 -0800

    Generics, broken

--- drawdown_rust/montecarlo.rs
+++ drawdown_rust/montecarlo.rs
@@ -1,11 +1,12 @@
 use std::iter::empty;
 use std::{fs::File, collections::BTreeMap};
 use std::io::prelude::*;
-use std::ops::{Range, Rem, Add, Sub, Div};
+use std::ops::{Range, Rem, Add, Sub, Div, Mul};
 use std::sync::mpsc;
 use std::{thread, result};
 
 use rand::prelude::*;
+use serde::de::IntoDeserializer;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -188,8 +189,11 @@ fn compute_simulation(simulation_config: &SimulationConfig, historical_data: &Ve
 
 }
 
-fn determine_parallel_ranges(data_range: Range<usize>, degree_of_parallelism: usize) -> Vec::<Range<usize>> {
-    // Distribute the remainder across the first few threads.
+fn determine_parallel_ranges<T: Ord+Eq+Sub<Output=T>+Add<Output=T>+Div<Output=T>+Mul<Output=T>+Rem<Output=T>+Into<usize>+From<usize>, 
+    U: Into<T> + Into<usize>>
+    (data_range: Range<T>, degree_of_parallelism: U) -> Vec::<Range<T>> {
+    
+        // Distribute the remainder across the first few threads.
     // E.g. 50 units of work / 8 threads = 6.25 years per thread, or 6R2. 
     // The first n (n=remainder) threads get an additional unit of work:
     // 0..7, 7..14, and the rest of the threads get 6 years 
@@ -197,14 +201,17 @@ fn determine_parallel_ranges(data_range: Range<usize>, degree_of_parallelism: us
 
     let size_of_work = data_range.end - data_range.start;
     // If there is less work than threads available
-    let degree_of_parallelism = size_of_work.min(degree_of_parallelism);
-    let vals_per_thread = size_of_work / degree_of_parallelism;
-    let remainder = size_of_work % degree_of_parallelism;
+    let dop_u: usize = degree_of_parallelism.into();
+    let dop_u: usize = dop_u.min(size_of_work.into());
+    let dop_t: T = degree_of_parallelism.into();
+    let vals_per_thread = size_of_work / dop_t;
+    let remainder = size_of_work % dop_t;
     
-    let mut ranges = Vec::<Range<usize>>::with_capacity(degree_of_parallelism);
-    for thread_num in 0..degree_of_parallelism {
-        let range_start = (thread_num*vals_per_thread) + thread_num.min(remainder);
-        let range_end = ((thread_num+1)*vals_per_thread) + (thread_num + 1).min(remainder);
+    let mut ranges = Vec::<Range<T>>::with_capacity(dop_u);
+    for thread_num in 0..dop_u {
+        let thread_num = T::from(thread_num);
+        let range_start = data_range.start + (thread_num*vals_per_thread) + (thread_num.min(remainder));
+        let range_end = data_range.start + ((thread_num + T::from(1))*vals_per_thread) + (thread_num + T::from(1)).min(remainder);
         ranges.push(range_start .. range_end);
     }
 
@@ -268,6 +275,9 @@ pub fn simulation(simulation_config: SimulationConfig) -> std::io::Result<()> {
     assert_eq!(determine_parallel_ranges(0..50, 8), vec![0..7, 7..14, 14..20, 20..26, 26..32, 32..38, 38..44, 44..50]);
     assert_eq!(determine_parallel_ranges(0..3, 4), vec![0..1, 1..2, 2..3]);
     assert_eq!(determine_parallel_ranges(0..7, 4), vec![0..2, 2..4, 4..6, 6..7]);
+    assert_eq!(determine_parallel_ranges(10..17, 4), vec![10..12, 12..14, 14..16, 16..17]);
+    
+
 
     let mut file = File::open("../data/historicalMarketData.json")?;
     let mut contents = String::new();
